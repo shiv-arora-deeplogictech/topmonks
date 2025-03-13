47fa074f52d47b3f3d9923eac47e399f32ae0989
Nishant-Bindhani <bindhaninishant12@gmail.com>
2025-03-13 13:32:37 +0530

    patch request allowed in cors

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ const server = https.createServer(options,(req, res) => {
     // Set CORS Headers
     
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Content-Type","application/json" );
 
